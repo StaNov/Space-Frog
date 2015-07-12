9cf26cb4ed32fb6dbf853a39ea1d9c3716d8aa91
StaNov <stanovec@gmail.com>
2015-07-12 17:06:31 +0200

    initial commit

---
+++ One Button Platformer/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rigidBody;
+	private Transform bottom;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody2D>();
+		bottom = transform.FindChild("bottom");
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && StandingOnFloor()) {
+			rigidBody.AddForce(new Vector2(5,5), ForceMode2D.Impulse);
+		}
+	}
+
+	private bool StandingOnFloor() {
+		RaycastHit2D hit = Physics2D.Raycast(bottom.position, -Vector2.up, 0.1f);
+		return hit.collider != null;
+	}
+}
