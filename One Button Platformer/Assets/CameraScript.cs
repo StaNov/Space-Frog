ad32e9b7f66e2cf0ec2e368495d0d3c87ced515c
StaNov <stanovec@gmail.com>
2015-07-27 22:29:14 +0200

    pohyb kamery, resolves #9

---
+++ One Button Platformer/Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform player;
+	public float rightOffset;
+
+	void Update () {
+		Vector3 targetPosition = player.position + new Vector3(rightOffset,0,-1);
+
+		if (Vector3.Distance(transform.position, targetPosition) < 0.01) {
+			transform.position = targetPosition;
+		} else {
+			transform.position = Vector3.Lerp (transform.position, targetPosition, 0.01f);
+		}
+	}
+}
