7f21a7c4694b56c541111afbac6df87173265cd4
StaNov <stanovec@gmail.com>
2015-07-29 21:53:29 +0200

    escapem se zavírá

---
+++ One Button Platformer/Assets/scripts/DesktopCloser.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DesktopCloser : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
