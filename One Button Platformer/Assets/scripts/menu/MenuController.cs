628d7a9ff378dc2cf79681abf545a9f82bbfb147
StaNov <stanovec@gmail.com>
2015-08-10 17:44:43 +0200

    resolves #8, menu

---
+++ One Button Platformer/Assets/scripts/menu/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	
+	}
+}
